dd9d5a530f3e0ce3c5a2a1819c1385a9c3d60a67
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-09-26 02:46:02 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
             return handleError('All fields are required'); 
         }
         try{
-            const url="http://localhost:8080/auth/login";
+            const url="https://auth-mern-app-api-three.vercel.app/auth/login";
             const response=await fetch(url,{
                 method:"POST",
                 headers:{
